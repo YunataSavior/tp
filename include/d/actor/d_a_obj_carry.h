b2f73598121b6ef430a0b0d327f7d613bd44b48c
YunataSavior <yunatasavior@gmail.com>
2025-01-23 17:55:48 -0800

    Initial d_a_obj_Y_taihou work

--- include/d/actor/d_a_obj_carry.h
+++ include/d/actor/d_a_obj_carry.h
@@ -202,6 +202,7 @@ public:
     void startCtrl() { mCtrl = 1; }
     void endCtrl() { mCtrl = 0; }
     void setDrop() { field_0xcf2 = 3; }
+    void offDraw() { mDraw = 1; }
 
     static void make_prm(csXyz* param_1, u8 param_2, u8 param_3, u8 param_4, u8 param_5, u8 param_6=0) {
         param_1->x = (param_4 << 8) | param_3;
--- src/d/actor/d_a_obj_Y_taihou.cpp
+++ src/d/actor/d_a_obj_Y_taihou.cpp
@@ -7,6 +7,8 @@
 #include "d/d_cc_d.h"
 #include "dol2asm.h"
 #include "d/actor/d_a_obj_carry.h"
+#include "Z2AudioLib/Z2Instances.h"
+#include "SSystem/SComponent/c_math.h"
 
 //
 // Forward References:
@@ -156,27 +158,30 @@ static void ccHitCallback(fopAc_ac_c* param_0, dCcD_GObjInf* param_1, fopAc_ac_c
     // NONMATCHING
 }
 
-/* ############################################################################################## */
-/* 80BA1164-80BA1168 000010 0004+00 1/3 0/0 0/0 .rodata          @3733 */
-SECTION_RODATA static u8 const lit_3733[4] = {
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-};
-COMPILER_STRIP_GATE(0x80BA1164, &lit_3733);
-
 /* 80B9FC40-80B9FD20 000240 00E0+00 1/1 0/0 0/0 .text
  * pushPullcallBack__FP10fopAc_ac_cP10fopAc_ac_csQ29dBgW_Base13PushPullLabel */
-static void pushPullcallBack(fopAc_ac_c* param_0, fopAc_ac_c* param_1, s16 param_2,
-                                 dBgW_Base::PushPullLabel param_3) {
+static daObjYtaihou_c* pushPullcallBack(fopAc_ac_c* param_0, fopAc_ac_c* param_1, s16 param_2,
+                                        dBgW_Base::PushPullLabel label) {
     // NONMATCHING
-}
-
-/* 80B9FD20-80B9FD5C 000320 003C+00 1/1 0/0 0/0 .text            __dt__4cXyzFv */
-// cXyz::~cXyz() {
-extern "C" void __dt__4cXyzFv() {
-    // NONMATCHING
+    if (cLib_checkBit(label, dBgW_Base::PPLABEL_PUSH) != 0) {
+        cXyz my_vec = param_1->current.pos - param_0->current.pos;
+        mDoMtx_stack_c::YrotS(-param_1->shape_angle.y);
+        mDoMtx_stack_c::multVec(&my_vec, &my_vec);
+        u8 my_ubit;
+        if (my_vec.x <= 0.0f) {
+            my_ubit = -1;
+        } else {
+            my_ubit = 1;
+        }
+        if (cLib_distanceAngleS(param_0->current.pos.y, param_2) > 0x4000) {
+            my_ubit = -my_ubit;
+        }
+        if (cLib_checkBit(label, dBgW_Base::PPLABEL_PUSH) != 0) {
+            my_ubit = -my_ubit;
+        }
+        static_cast<daObjYtaihou_c*>(param_0)->setAddAngle(my_ubit);
+    }
+    return (daObjYtaihou_c*) param_0;
 }
 
 /* ############################################################################################## */
@@ -185,44 +190,70 @@ SECTION_RODATA static f32 const lit_3749 = 40000.0f;
 COMPILER_STRIP_GATE(0x80BA1168, &lit_3749);
 
 /* 80B9FD5C-80B9FDE8 00035C 008C+00 1/1 0/0 0/0 .text            searchIronBallCallback__FPvPv */
-static void searchIronBallCallback(void* param_0, void* param_1) {
+static void* searchIronBallCallback(void* param_0, void* param_1) {
     // NONMATCHING
 }
 
-/* ############################################################################################## */
-/* 80BA11D4-80BA11D4 000080 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
-#pragma push
-#pragma force_active on
-SECTION_DEAD static char const* const stringBase_80BA11D4 = "Y_taihou";
-#pragma pop
-
 /* 80BA11E0-80BA11E4 -00001 0004+00 3/3 0/0 0/0 .data            l_arcName */
-SECTION_DATA static void* l_arcName = (void*)&d_a_obj_Y_taihou__stringBase0;
+SECTION_DATA static char const* l_arcName[1] = {"Y_taihou"};
 
 /* 80B9FDE8-80B9FEB0 0003E8 00C8+00 1/1 0/0 0/0 .text            create1st__14daObjYtaihou_cFv */
-void daObjYtaihou_c::create1st() {
-    // NONMATCHING
+int daObjYtaihou_c::create1st() {
+    int phase = dComIfG_resLoad(this, l_arcName[0]);
+    if (phase == cPhs_COMPLEATE_e) {
+        setMtx();
+        phase = MoveBGCreate(l_arcName[0], 7, dBgS_MoveBGProc_TypicalRotY, 0x4550, &mMtx);
+        if (phase == cPhs_ERROR_e) {
+            return phase;
+        }
+        loadAngle();
+        setNextAngle();
+        shape_angle.x = 0;
+        shape_angle.y = current.angle.z;
+        fopAcM_Search(searchIronBallCallback,this);
+        // FIXME: Need mDoHIO_entry_c::entryHIO
+    }
+    return phase;
 }
 
 /* 80B9FEB0-80B9FF08 0004B0 0058+00 3/3 0/0 0/0 .text
  * setIronBall__14daObjYtaihou_cFP12daObjCarry_c                */
-void daObjYtaihou_c::setIronBall(daObjCarry_c* param_0) {
-    // NONMATCHING
+void daObjYtaihou_c::setIronBall(daObjCarry_c* obj) {
+    field_0x764 = fopAcM_GetID(obj);
+    obj->offDraw();
+    obj->startCtrl();
+    obj->current.pos = current.pos;
+    obj->old.pos = current.pos;
 }
 
 /* 80B9FF08-80B9FF1C 000508 0014+00 1/1 0/0 0/0 .text            getEvent__14daObjYtaihou_cFUc */
-void daObjYtaihou_c::getEvent(u8 param_0) {
-    // NONMATCHING
+int daObjYtaihou_c::getEvent(u8 param_0) {
+    return fopAcM_GetParamBit(this, (param_0 & 0x1f) << 3, 8);
 }
 
 /* 80B9FF1C-80B9FFAC 00051C 0090+00 1/1 0/0 0/0 .text            loadAngle__14daObjYtaihou_cFv */
 void daObjYtaihou_c::loadAngle() {
-    // NONMATCHING
+    int sw = home.angle.x >> 8 & 0xff;
+    field_0x775 = (fopAcM_isSwitch(this, home.angle.x & 0xff)) ? 2 : 0;
+    if (fopAcM_isSwitch(this, sw)) {
+        field_0x775 |= 1;
+    }
 }
 
 /* 80B9FFAC-80BA0060 0005AC 00B4+00 1/1 0/0 0/0 .text            saveAngle__14daObjYtaihou_cFv */
 void daObjYtaihou_c::saveAngle() {
-    // NONMATCHING
+    int sw_1 = home.angle.x & 0xff;
+    int sw_2 = home.angle.x >> 8 & 0xff;
+    if (field_0x775 & 2) {
+        fopAcM_onSwitch(this, sw_1);
+    } else {
+        fopAcM_offSwitch(this, sw_1);
+    }
+    if (field_0x775 & 1) {
+        fopAcM_onSwitch(this, sw_2);
+    } else {
+        fopAcM_offSwitch(this, sw_2);
+    }
 }
 
 /* ############################################################################################## */
@@ -242,63 +273,14 @@ static dCcD_SrcCyl l_cc_cyl_src = {
 };
 
 /* 80BA1228-80BA1230 000048 0008+00 1/1 0/0 0/0 .data            l_offsetAngle$3829 */
-SECTION_DATA static u8 l_offsetAngle[8] = {
-    0x00, 0x00, 0xC0, 0x00, 0x80, 0x00, 0x40, 0x00,
+static u16 l_offsetAngle[4] = {
+    0x0, 0xC000, 0x8000, 0x4000,
 };
 
 /* 80BA0060-80BA0084 000660 0024+00 2/2 0/0 0/0 .text            setNextAngle__14daObjYtaihou_cFv */
 void daObjYtaihou_c::setNextAngle() {
-    // NONMATCHING
+    current.angle.z = home.angle.y + l_offsetAngle[field_0x775];
 }
-
-/* ############################################################################################## */
-/* 80BA116C-80BA1170 000018 0004+00 0/1 0/0 0/0 .rodata          @3852 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3852 = 4.0f / 5.0f;
-COMPILER_STRIP_GATE(0x80BA116C, &lit_3852);
-#pragma pop
-
-/* 80BA1170-80BA1174 00001C 0004+00 0/1 0/0 0/0 .rodata          @3853 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3853 = 60.0f;
-COMPILER_STRIP_GATE(0x80BA1170, &lit_3853);
-#pragma pop
-
-/* 80BA1174-80BA1178 000020 0004+00 0/1 0/0 0/0 .rodata          @3854 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3854 = 59.0f;
-COMPILER_STRIP_GATE(0x80BA1174, &lit_3854);
-#pragma pop
-
-/* 80BA1178-80BA117C 000024 0004+00 0/1 0/0 0/0 .rodata          @3855 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3855 = 3.0f / 5.0f;
-COMPILER_STRIP_GATE(0x80BA1178, &lit_3855);
-#pragma pop
-
-/* 80BA117C-80BA1184 000028 0004+04 0/1 0/0 0/0 .rodata          @3856 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3856[1 + 1 /* padding */] = {
-    2.0f / 5.0f,
-    /* padding */
-    0.0f,
-};
-COMPILER_STRIP_GATE(0x80BA117C, &lit_3856);
-#pragma pop
-
-/* 80BA1184-80BA118C 000030 0008+00 0/1 0/0 0/0 .rodata          @3858 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static u8 const lit_3858[8] = {
-    0x43, 0x30, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00,
-};
-COMPILER_STRIP_GATE(0x80BA1184, &lit_3858);
-#pragma pop
 
 /* 80BA1318-80BA131C 000008 0001+03 1/1 0/0 0/0 .bss             @1109 */
 static u8 lit_1109[1 + 3 /* padding */];
@@ -394,14 +376,28 @@ static u8 lit_1009[1 + 3 /* padding */];
 #pragma pop
 
 /* 80BA1358-80BA135C 000048 0004+00 1/1 0/0 0/0 .bss             l_wheelMinR$3836 */
-static u8 l_wheelMinR[4];
+static f32 l_wheelMinR;
 
 /* 80BA135C-80BA1360 00004C 0004+00 1/1 0/0 0/0 .bss             None */
-static u8 data_80BA135C[4];
+static u8 lbl_396_bss_4C;
 
 /* 80BA0084-80BA0208 000684 0184+00 2/2 0/0 0/0 .text            setMtx__14daObjYtaihou_cFv */
 void daObjYtaihou_c::setMtx() {
     // NONMATCHING
+    home.angle.z += (s16) ((s16)(shape_angle.y - old.angle.y) * 0.8f);
+    if ((s8)lbl_396_bss_4C == 0) {
+        l_wheelMinR = cM_scos(0xccc) * 60.0f;
+        lbl_396_bss_4C = 1;
+    }
+    int iVar1 = abs(home.angle.z % 0x1999) - 0xccc;
+    f32 my_val = 59.0f - l_wheelMinR / cM_scos(iVar1);
+    speedF += speed.y;
+    speed.y *= 0.6f;
+    speed.y -= speedF * 0.4f;
+    mDoMtx_stack_c::transS(current.pos.x, current.pos.y - my_val, current.pos.z);
+    mDoMtx_stack_c::YrotM(shape_angle.y);
+    mDoMtx_stack_c::transM(0.0f, 0.0f, speedF);
+    mDoMtx_copy(mDoMtx_stack_c::get(), mMtx);
 }
 
 /* ############################################################################################## */
@@ -503,12 +499,12 @@ void daObjYtaihou_c::eventStart() {
 }
 
 /* 80BA0974-80BA09E4 000F74 0070+00 1/0 0/0 0/0 .text            CreateHeap__14daObjYtaihou_cFv */
-void daObjYtaihou_c::CreateHeap() {
+int daObjYtaihou_c::CreateHeap() {
     // NONMATCHING
 }
 
 /* 80BA09E4-80BA0A94 000FE4 00B0+00 1/0 0/0 0/0 .text            Create__14daObjYtaihou_cFv */
-void daObjYtaihou_c::Create() {
+int daObjYtaihou_c::Create() {
     // NONMATCHING
 }
 
@@ -521,17 +517,17 @@ COMPILER_STRIP_GATE(0x80BA11C8, &l_cc_offset);
 
 /* 80BA0A94-80BA0B4C 001094 00B8+00 1/0 0/0 0/0 .text            Execute__14daObjYtaihou_cFPPA3_A4_f
  */
-void daObjYtaihou_c::Execute(f32 (**param_0)[3][4]) {
+int daObjYtaihou_c::Execute(f32 (**param_0)[3][4]) {
     // NONMATCHING
 }
 
 /* 80BA0B4C-80BA0C1C 00114C 00D0+00 1/0 0/0 0/0 .text            Draw__14daObjYtaihou_cFv */
-void daObjYtaihou_c::Draw() {
+int daObjYtaihou_c::Draw() {
     // NONMATCHING
 }
 
 /* 80BA0C1C-80BA0C88 00121C 006C+00 1/0 0/0 0/0 .text            Delete__14daObjYtaihou_cFv */
-void daObjYtaihou_c::Delete() {
+int daObjYtaihou_c::Delete() {
     // NONMATCHING
 }
 
@@ -716,176 +712,6 @@ static void func_80BA113C() {
     // NONMATCHING
 }
 
-/* ############################################################################################## */
-/* 80BA137C-80BA1380 00006C 0004+00 0/0 0/0 0/0 .bss
- * sInstance__40JASGlobalInstance<19JASDefaultBankTable>        */
-#pragma push
-#pragma force_active on
-static u8 data_80BA137C[4];
-#pragma pop
-
-/* 80BA1380-80BA1384 000070 0004+00 0/0 0/0 0/0 .bss
- * sInstance__35JASGlobalInstance<14JASAudioThread>             */
-#pragma push
-#pragma force_active on
-static u8 data_80BA1380[4];
-#pragma pop
-
-/* 80BA1384-80BA1388 000074 0004+00 0/0 0/0 0/0 .bss sInstance__27JASGlobalInstance<7Z2SeMgr> */
-#pragma push
-#pragma force_active on
-static u8 data_80BA1384[4];
-#pragma pop
-
-/* 80BA1388-80BA138C 000078 0004+00 0/0 0/0 0/0 .bss sInstance__28JASGlobalInstance<8Z2SeqMgr> */
-#pragma push
-#pragma force_active on
-static u8 data_80BA1388[4];
-#pragma pop
-
-/* 80BA138C-80BA1390 00007C 0004+00 0/0 0/0 0/0 .bss sInstance__31JASGlobalInstance<10Z2SceneMgr>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA138C[4];
-#pragma pop
-
-/* 80BA1390-80BA1394 000080 0004+00 0/0 0/0 0/0 .bss sInstance__32JASGlobalInstance<11Z2StatusMgr>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA1390[4];
-#pragma pop
-
-/* 80BA1394-80BA1398 000084 0004+00 0/0 0/0 0/0 .bss sInstance__31JASGlobalInstance<10Z2DebugSys>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA1394[4];
-#pragma pop
-
-/* 80BA1398-80BA139C 000088 0004+00 0/0 0/0 0/0 .bss
- * sInstance__36JASGlobalInstance<15JAISoundStarter>            */
-#pragma push
-#pragma force_active on
-static u8 data_80BA1398[4];
-#pragma pop
-
-/* 80BA139C-80BA13A0 00008C 0004+00 0/0 0/0 0/0 .bss
- * sInstance__35JASGlobalInstance<14Z2SoundStarter>             */
-#pragma push
-#pragma force_active on
-static u8 data_80BA139C[4];
-#pragma pop
-
-/* 80BA13A0-80BA13A4 000090 0004+00 0/0 0/0 0/0 .bss
- * sInstance__33JASGlobalInstance<12Z2SpeechMgr2>               */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13A0[4];
-#pragma pop
-
-/* 80BA13A4-80BA13A8 000094 0004+00 0/0 0/0 0/0 .bss sInstance__28JASGlobalInstance<8JAISeMgr> */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13A4[4];
-#pragma pop
-
-/* 80BA13A8-80BA13AC 000098 0004+00 0/0 0/0 0/0 .bss sInstance__29JASGlobalInstance<9JAISeqMgr> */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13A8[4];
-#pragma pop
-
-/* 80BA13AC-80BA13B0 00009C 0004+00 0/0 0/0 0/0 .bss
- * sInstance__33JASGlobalInstance<12JAIStreamMgr>               */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13AC[4];
-#pragma pop
-
-/* 80BA13B0-80BA13B4 0000A0 0004+00 0/0 0/0 0/0 .bss sInstance__31JASGlobalInstance<10Z2SoundMgr>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13B0[4];
-#pragma pop
-
-/* 80BA13B4-80BA13B8 0000A4 0004+00 0/0 0/0 0/0 .bss
- * sInstance__33JASGlobalInstance<12JAISoundInfo>               */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13B4[4];
-#pragma pop
-
-/* 80BA13B8-80BA13BC 0000A8 0004+00 0/0 0/0 0/0 .bss
- * sInstance__34JASGlobalInstance<13JAUSoundTable>              */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13B8[4];
-#pragma pop
-
-/* 80BA13BC-80BA13C0 0000AC 0004+00 0/0 0/0 0/0 .bss
- * sInstance__38JASGlobalInstance<17JAUSoundNameTable>          */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13BC[4];
-#pragma pop
-
-/* 80BA13C0-80BA13C4 0000B0 0004+00 0/0 0/0 0/0 .bss
- * sInstance__33JASGlobalInstance<12JAUSoundInfo>               */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13C0[4];
-#pragma pop
-
-/* 80BA13C4-80BA13C8 0000B4 0004+00 0/0 0/0 0/0 .bss sInstance__32JASGlobalInstance<11Z2SoundInfo>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13C4[4];
-#pragma pop
-
-/* 80BA13C8-80BA13CC 0000B8 0004+00 0/0 0/0 0/0 .bss
- * sInstance__34JASGlobalInstance<13Z2SoundObjMgr>              */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13C8[4];
-#pragma pop
-
-/* 80BA13CC-80BA13D0 0000BC 0004+00 0/0 0/0 0/0 .bss sInstance__31JASGlobalInstance<10Z2Audience>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13CC[4];
-#pragma pop
-
-/* 80BA13D0-80BA13D4 0000C0 0004+00 0/0 0/0 0/0 .bss sInstance__32JASGlobalInstance<11Z2FxLineMgr>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13D0[4];
-#pragma pop
-
-/* 80BA13D4-80BA13D8 0000C4 0004+00 0/0 0/0 0/0 .bss sInstance__31JASGlobalInstance<10Z2EnvSeMgr>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13D4[4];
-#pragma pop
-
-/* 80BA13D8-80BA13DC 0000C8 0004+00 0/0 0/0 0/0 .bss sInstance__32JASGlobalInstance<11Z2SpeechMgr>
- */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13D8[4];
-#pragma pop
-
-/* 80BA13DC-80BA13E0 0000CC 0004+00 0/0 0/0 0/0 .bss
- * sInstance__34JASGlobalInstance<13Z2WolfHowlMgr>              */
-#pragma push
-#pragma force_active on
-static u8 data_80BA13DC[4];
-#pragma pop
+AUDIO_INSTANCES;
 
 /* 80BA11D4-80BA11D4 000080 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
